28bc93ffdd4bc123bfc379cdffb52a84b342ec8e
Giovanni Duarte <garduarte94@gmail.com>
2026-04-14 22:58:52 -0300

    adicionando script para criação de clientes na base de dados

---
+++ src/main/resources/db/migration/V2__script-clientes.sql
@@ -0,0 +1,13 @@
+INSERT INTO
+    clientes (nome, documento, email, perfil_risco, data_criacao, data_atualizacao)
+VALUES
+    ('Ana Silva',        '33838935020', 'ana.silva@mail.com',        'CONSERVADOR',      SYSUTCDATETIME(), SYSUTCDATETIME()),
+    ('Bruno Souza',      '42246887003', 'bruno.souza@mail.com',      'MODERADO',         SYSUTCDATETIME(), SYSUTCDATETIME()),
+    ('Carla Lima',       '73087951013', 'carla.lima@mail.com',       'ARROJADO',         SYSUTCDATETIME(), SYSUTCDATETIME()),
+    ('Diego Santos',     '95444357003', 'diego.santos@mail.com',     'AGRESSIVO',        SYSUTCDATETIME(), SYSUTCDATETIME()),
+    ('Eduarda Rocha',    '63387930097', 'eduarda.rocha@mail.com',    'NAO_CLASSIFICADO', SYSUTCDATETIME(), SYSUTCDATETIME()),
+    ('Felipe Almeida',   '39033322080', 'felipe.almeida@mail.com',   'CONSERVADOR',      SYSUTCDATETIME(), SYSUTCDATETIME()),
+    ('Gabriela Nunes',   '34248244020', 'gabriela.nunes@mail.com',   'MODERADO',         SYSUTCDATETIME(), SYSUTCDATETIME()),
+    ('Henrique Costa',   '33574465025', 'henrique.costa@mail.com',   'ARROJADO',         SYSUTCDATETIME(), SYSUTCDATETIME()),
+    ('Isabela Martins',  '68480925094', 'isabela.martins@mail.com',  'AGRESSIVO',        SYSUTCDATETIME(), SYSUTCDATETIME()),
+    ('Joao Pereira',     '86876197077', 'joao.pereira@mail.com',     'NAO_CLASSIFICADO', SYSUTCDATETIME(), SYSUTCDATETIME());
