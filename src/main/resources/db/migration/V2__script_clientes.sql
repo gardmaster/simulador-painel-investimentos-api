33e6ca15b1deb260f85302097ddb4e424fd5a84b
Giovanni Duarte <garduarte94@gmail.com>
2026-04-24 19:23:54 -0300

    ajustante recursos de cliente, atualizando scripts e realm exportado do keycloak

--- src/main/resources/db/migration/V2__script_clientes.sql
+++ src/main/resources/db/migration/V2__script_clientes.sql
@@ -1,13 +1,8 @@
 INSERT INTO
-    clientes (nome, documento, email, perfil_risco, data_criacao, data_atualizacao)
+    clientes (auth_user_id, nome, documento, email, perfil_risco, data_criacao, data_atualizacao)
 VALUES
-    ('Ana Silva',        '33838935020', 'ana.silva@mail.com',        'CONSERVADOR',      SYSUTCDATETIME(), SYSUTCDATETIME()),
-    ('Bruno Souza',      '42246887003', 'bruno.souza@mail.com',      'MODERADO',         SYSUTCDATETIME(), SYSUTCDATETIME()),
-    ('Carla Lima',       '73087951013', 'carla.lima@mail.com',       'ARROJADO',         SYSUTCDATETIME(), SYSUTCDATETIME()),
-    ('Diego Santos',     '95444357003', 'diego.santos@mail.com',     'AGRESSIVO',        SYSUTCDATETIME(), SYSUTCDATETIME()),
-    ('Eduarda Rocha',    '63387930097', 'eduarda.rocha@mail.com',    'NAO_CLASSIFICADO', SYSUTCDATETIME(), SYSUTCDATETIME()),
-    ('Felipe Almeida',   '39033322080', 'felipe.almeida@mail.com',   'CONSERVADOR',      SYSUTCDATETIME(), SYSUTCDATETIME()),
-    ('Gabriela Nunes',   '34248244020', 'gabriela.nunes@mail.com',   'MODERADO',         SYSUTCDATETIME(), SYSUTCDATETIME()),
-    ('Henrique Costa',   '33574465025', 'henrique.costa@mail.com',   'ARROJADO',         SYSUTCDATETIME(), SYSUTCDATETIME()),
-    ('Isabela Martins',  '68480925094', 'isabela.martins@mail.com',  'AGRESSIVO',        SYSUTCDATETIME(), SYSUTCDATETIME()),
-    ('Joao Pereira',     '86876197077', 'joao.pereira@mail.com',     'NAO_CLASSIFICADO', SYSUTCDATETIME(), SYSUTCDATETIME());
+    ('f2337392-38e0-4448-bc12-aad58e4f84e6', 'Ana Silva',        '33838935020', 'ana.silva@mail.com',        'CONSERVADOR',      SYSUTCDATETIME(), SYSUTCDATETIME()),
+    ('28c0a5c6-0a5b-4f5b-afab-9e8942a9ab4a', 'Bruno Souza',      '42246887003', 'bruno.souza@mail.com',      'MODERADO',         SYSUTCDATETIME(), SYSUTCDATETIME()),
+    ('e86c5cd7-5418-4a52-96f6-81575c70cfbd', 'Carla Lima',       '73087951013', 'carla.lima@mail.com',       'ARROJADO',         SYSUTCDATETIME(), SYSUTCDATETIME()),
+    ('3db34847-4663-456f-a185-15b4536b3209', 'Diego Santos',     '95444357003', 'diego.santos@mail.com',     'AGRESSIVO',        SYSUTCDATETIME(), SYSUTCDATETIME()),
+    ('58b700de-f05e-41ff-9dd9-8b0983a9320d', 'Eduarda Rocha',    '63387930097', 'eduarda.rocha@mail.com',    'NAO_CLASSIFICADO', SYSUTCDATETIME(), SYSUTCDATETIME());
