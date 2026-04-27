abb62884b4d0091bc2568185d7dae0c0e5db6433
Giovanni Duarte <garduarte94@gmail.com>
2026-04-27 19:43:07 -0300

    criando v3 script produtos

---
+++ src/main/resources/db/migration/V3__script_produtos.sql
@@ -0,0 +1,16 @@
+INSERT INTO
+    produtos (nome, tipo_produto, produto_risco, rentabilidade_mensal, data_criacao, data_atualizacao)
+VALUES
+    ('CDB Banco Caixa', 'CDB', 'BAIXO', 1.0, GETDATE(), GETDATE()),
+    ('LCI Banco Bradesco', 'LCI', 'BAIXO', 0.9, GETDATE(), GETDATE()),
+    ('LCA Banco Santander', 'LCA', 'BAIXO', 0.85, GETDATE(), GETDATE()),
+    ('Tesouro Direto Selic', 'TESOURO_DIRETO_SELIC', 'BAIXISSIMO', 1.0, GETDATE(), GETDATE()),
+    ('FIA Fundo de Ações Suno Master', 'FIA', 'ALTO', 1.5, GETDATE(), GETDATE()),
+    ('FII Fundo Imobiliário Kinea', 'FII', 'MEDIO', 1.2, GETDATE(), GETDATE()),
+    ('CRI CRI XP', 'CRI', 'ALTO', 1.3, GETDATE(), GETDATE()),
+    ('CRA CRA Itaú', 'CRA', 'ALTO', 1.4, GETDATE(), GETDATE()),
+    ('CDB Banco Master', 'CDB', 'ALTISSIMO', 2.0, GETDATE(), GETDATE()),
+    ('LCI Banco Inter', 'LCI', 'MEDIO', 1.2, GETDATE(), GETDATE());
+
+
+
