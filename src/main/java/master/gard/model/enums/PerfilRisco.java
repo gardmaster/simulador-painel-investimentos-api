b47e8bacdf7f5a5c12d9565bf4dde39f35dfea82
Giovanni Duarte <garduarte94@gmail.com>
2026-05-09 19:52:05 -0300

    ajustando perfil de risco do investidor

--- src/main/java/master/gard/model/enums/PerfilRisco.java
+++ src/main/java/master/gard/model/enums/PerfilRisco.java
@@ -11,13 +11,7 @@ public enum PerfilRisco {
     @Schema(description = "Investidor moderado, com tolerância média a riscos e busca por equilíbrio entre segurança e retorno")
     MODERADO,
 
-    @Schema(description = "Investidor arrojado, com alta tolerância a riscos e foco em potencial de retorno elevado")
-    ARROJADO,
-
     @Schema(description = "Investidor agressivo, com altíssima tolerância a riscos e busca por retornos máximos, mesmo que isso implique em grandes oscilações")
-    AGRESSIVO,
-
-    @Schema(description = "Perfil de risco não classificado, utilizado quando não é possível determinar o perfil do investidor")
-    NAO_CLASSIFICADO;
+    AGRESSIVO;
 
 }
